efd9dfc3d78eb43bc2e5da10d5c393caf14d2559
jrs <372717320@qq.com>
2019-08-02 19:41:38 +0800

    commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Jtpl必须用到的loader
-    通过正则解析jtpl模板，分离出html模块和javacsript脚本
+    解析jtpl模板，分离出html模块和javacsript脚本
     并生成scope，创建模块实例，生成模板内容
--- jtpl-loader.js
+++ jtpl-loader.js
@@ -1,5 +1,5 @@
 /**
- * 通过正则解析jtpl模板，分离出html模块和javacsript脚本
+ * 解析jtpl模板，分离出html模块和javacsript脚本
  * 并生成scope，创建模块实例，生成模板内容
  * @author jrs
  */
